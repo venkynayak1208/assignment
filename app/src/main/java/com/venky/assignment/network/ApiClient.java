eed75c7c061770f6ea61cbe77daf17463e6706bf
venkatesh <venkatesh.hk1208@gmail.com>
2020-08-17 17:47:02 +0530

    Assignment =>Created Alert to show image description.

--- app/src/main/java/com/venky/assignment/network/ApiClient.java
+++ app/src/main/java/com/venky/assignment/network/ApiClient.java
@@ -19,7 +19,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * Created by Venky on 09-Oct-19.
+ * Created by Venky on 17-Aug-20.
  */
 
 public class ApiClient {
--- app/src/main/java/com/venky/assignment/network/RestService.java
+++ app/src/main/java/com/venky/assignment/network/RestService.java
@@ -10,7 +10,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 /**
- * Created by Venky on 09-Oct-19.
+ * Created by Venky on 17-Aug-20.
  */
 
 public interface RestService {
